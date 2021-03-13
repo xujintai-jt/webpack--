adeb1cdf401d35a0c52623b811cfe06957be9d90
xujintai-jt <1791639435@qq.com>
2021-03-13 20:22:36 +0800

    webpack练习文件夹目录整理

--- 02_webpack_css/webpack.config.js
+++ 02_webpack_css/webpack.config.js
@@ -1,19 +1,24 @@
-const { resolve } = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// commonjs
+//引入核心模块
+const { resolve } = require("path");
+//引入插件(第三方插件)
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+// commonjs
+// const exports = module.exports
 module.exports = {
-  mode: "development",
-  entry: resolve(__dirname,'src/index.js'),
+  //样式资源也在index.js(入口文件)中
+  entry: "./src/index.js",
   output: {
-    path: resolve(__dirname, "dist"), // string
-    filename: "bundle.js", // string
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
-    // 关于模块配置
+    //不同的文件配置不同的loader
     rules: [
-      // 模块规则（配置 loader、解析器等选项）
       {
         test: /\.css$/,
+        //use数组中loader执行顺序从右向左，从下到上，依次执行
         use: [
           //创建style标签，将js中的样式资源插入进行，添加到head中生效。
           "style-loader",
@@ -24,17 +29,20 @@ module.exports = {
       {
         test: /\.less$/,
         use: [
-          //创建style标签，将js中的样式资源插入进行，添加到head中生效。
-          "style-loader",
-          //将css文件变成commonjs模块加载js中，里面内容是样式字符串。
-          "css-loader",
-          "less-loader",
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          //将less文件编译成css文件
+          "less-loader", // compiles Less to CSS
         ],
-      }
-    ]
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template:resolve(__dirname,'src/index.html')
-  })]
-  
-}
+  plugins: [
+    //自动引入打包输出所有的资源
+    new HtmlWebpackPlugin({
+      //复制本地src/index.html文件(不需要自己引入打包资源)，引入打包输出所有的资源
+      template: resolve("./src/index.html"),
+    }),
+  ],
+  mode: "development",
+};
