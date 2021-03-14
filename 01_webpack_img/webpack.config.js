cdfebea63c7f6f44ed98d86f98f16c861ff53300
xujintai-jt <1791639435@qq.com>
2021-03-14 17:00:32 +0800

    使用html-loader处理html中使用img问题,不显示图片的bug修改esModule解决

--- 01_webpack_img/webpack.config.js
+++ 01_webpack_img/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     path: resolve(__dirname, "dist"), // string
     filename: "bundle.js", // string
+    // publicPath: "/dist/"
   },
   module: {
     rules: [{
@@ -20,6 +21,7 @@ module.exports = {
           loader: "less-loader" // compiles Less to CSS
       }]
     },
+      //问题：默认处理不了html中的图片
     {
       test: /\.(png|jpg|gif|JPG)$/,
       use: [
@@ -27,11 +29,32 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 8 * 1024,
-            esModule: false,
+            //url-loader(url图片)默认情况下，生成使用 ES 模块语法的 JS 模块。
+            //而低版本html-loader(html引入图片)使用commonjs
+            //解析时会出问题[Object Module]
+            //解决：关闭url-loader的es6 module，使用commonjs解析。或者使用新版本html-loader
+            // esModule: false,
+            //ext:取文件的原扩展名
+           name: '[hash:10].[ext]'
           }
-        }
+        },
       ]
-    }]
+      },
+    //html-loader 专门用来处理html文档中的img图片(负责引入图片，从而可以让url-loader处理)
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            //默认情况下，生成使用ES块语法的JS模块
+            //问题:生成的图片显示错误
+            //解决:esModule改为false
+            options: {
+              esModule: false,
+            },
+          },
+        ]
+      }]
   },
   plugins: [new HtmlWebpackPlugin(
     {
